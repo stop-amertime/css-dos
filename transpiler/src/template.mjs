03aba223ac8be4044213c05c7abdb0318c4a868f
Ahmed Amer <ahmed.elhadi.amer@gmail.com>
2026-04-17 17:06:18 +0100

    feat(transpiler): add arrow/Tab/Bksp buttons to HTML footer

--- transpiler/src/template.mjs
+++ transpiler/src/template.mjs
@@ -191,7 +191,7 @@ key-board button {
 <div class="clock" style="container-type:inline-size">
   <div class="cpu">
     <key-board>
-      <button>0</button><button>1</button><button>2</button><button>3</button><button>4</button><button>5</button><button>6</button><button>7</button><button>8</button><button>9</button><button>Q</button><button>W</button><button>E</button><button>R</button><button>T</button><button>Y</button><button>U</button><button>I</button><button>O</button><button>P</button><button>A</button><button>S</button><button>D</button><button>F</button><button>G</button><button>H</button><button>J</button><button>K</button><button>L</button><button>\u21B5</button><button>Z</button><button>X</button><button>C</button><button>V</button><button>B</button><button>N</button><button>M</button><button>\u2423</button><button>Esc</button>
+      <button>0</button><button>1</button><button>2</button><button>3</button><button>4</button><button>5</button><button>6</button><button>7</button><button>8</button><button>9</button><button>Q</button><button>W</button><button>E</button><button>R</button><button>T</button><button>Y</button><button>U</button><button>I</button><button>O</button><button>P</button><button>A</button><button>S</button><button>D</button><button>F</button><button>G</button><button>H</button><button>J</button><button>K</button><button>L</button><button>\u21B5</button><button>Z</button><button>X</button><button>C</button><button>V</button><button>B</button><button>N</button><button>M</button><button>\u2423</button><button>Esc</button><button>\u2190</button><button>\u2193</button><button>\u2191</button><button>\u2192</button><button>Tab</button><button>Bksp</button>
     </key-board>
   </div>
 </div>
